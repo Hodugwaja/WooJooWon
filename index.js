66811d8ce05296fa28972e36ba3d8cebceaa7005
HoduGwaja <honeybutter9250@gmail.com>
2021-02-16 08:36:16 +0900

    [commit | 2020.02.16 | added censorship, date funcitons]

---
+++ index.js
@@ -0,0 +1,60 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+const {prefix, token} = require('./config.json')
+var date = 253;
+
+
+client.on('message', (message) => {
+    if(message.author.bot) {
+        return NaN
+    }
+    if(message.content === "우주원 출소날짜"){
+        var today = new Date();
+        today.setDate(today.getDate() + date)
+        today.toLocaleString()
+        message.reply(`우주원의 출소일자는 ${today.getFullYear()}년 ${today.getMonth()}월 ${today.getDate()}일 입니다`);
+        return NaN;
+    }
+    if (!message.content.replace(/[\{\}\[\]\/?.,;:\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi, '').endsWith('냥')) {
+        if(message.author.id === "617184459250466827"){
+            date++;
+            const embed = new discord.MessageEmbed()
+            .setColor("#000000")
+            .setTitle("우주원 냥체 안씀")
+            .setDescription(`1일 추가 연장\n냥체 헤제하기 ${date}일 남음`)
+            .addFields(
+                {
+                    name : "내용",
+                    value : `${message.content}`,
+                },
+            )
+            .setTimestamp()
+            .setFooter("호두과자 #2021", "https://cdn.discordapp.com/avatars/533120411274182666/83b362e9916a243c201e186292209718.webp?size=128") 
+            message.reply(embed);
+        }
+    }
+})
+const activities_list = [
+    "made by hodugwaja", 
+    "우주원 감시",
+    `우주원 출소 ${date}일 남음`
+];
+
+
+
+function random(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+ 
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+client.on('ready', () => {
+    console.log(`logged in as ${client.user.tag} `);
+            
+    setInterval(() => {
+        const index = random(1, activities_list.length-1);
+        client.user.setActivity(activities_list[index]);
+    }, 10000); 
+});
+client.login(token);
