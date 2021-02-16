5b1a9573c4e0f6b6b87bd9137a97973549debd12
HoduGwaja <honeybutter9250@gmail.com>
2021-02-16 09:04:46 +0900

    [commit | 2020.02.16 | added readme on discord]

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const discord = require('discord.js');
 const client = new discord.Client();
 const {prefix, token} = require('./config.json')
 var date = 253;
-
+var Joowon = "617184459250466827"
 
 client.on('message', (message) => {
     var args = message.content.split(' ');
     if(message.author.bot) {
         return NaN
+    }
+    if(message.content === "우주원 Readme"){
+        const embedReadme = new discord.MessageEmbed()
+            .setColor("#000000")
+            .setTitle("우주원 봇 설명")
+            .setDescription(`우주원이 냥체 벌칙에 걸려서 이봇을 통해 남은 날짜를 표시하는 봇`)
+            .addFields(
+                {
+                    name : "우주원 출소날짜",
+                    value : `우주원이 냥체 풀리는 날을 출력`,
+                },
+                {
+                    name : '우주원 형량 추가 (숫자)',
+                    value : `우주원의 형량을 추가 가능(일부 인원)`,
+                },
+                {   
+                    name : '우주원 형량 감형 (숫자)',
+                    value : `우주원의 형량을 감형 가능(일부 인원)`
+                },
+            )
+            .setThumbnail(message.author.avatarURL())
+            .setTimestamp()
+            .setFooter("호두과자 #2021", "https://cdn.discordapp.com/avatars/533120411274182666/83b362e9916a243c201e186292209718.webp?size=128") 
+            message.reply(embedReadme);
+        return NaN;
     }
     if(message.content === "우주원 출소날짜"){
         var today = new Date();
@@ -48,7 +73,8 @@ client.on('message', (message) => {
 const activities_list = [
     "made by hodugwaja", 
     "우주원 감시",
-    `우주원 출소 ${date}일 남음`
+    `우주원 출소 ${date}일 남음`,
+    `**우주원 Readme** 를 통해 명령어 확인`
 ];
 
 
